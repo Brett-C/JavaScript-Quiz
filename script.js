ab4b10318c2f0613758f4a49c29f6a258949213a
Brett C <brcannon@bellsouth.net>
2023-08-29 21:34:01 -0500

    Created the questions and got them on the page

--- script.js
+++ script.js
@@ -1,23 +1,103 @@
 //Timer
-var timeLeft = 35;
-var time = document.getElementById('timer');
-var timerId = setInterval(countdown, 1000);
+let timeLeft = 60;
+const time = document.getElementById('timer');
+const timerId = setInterval(countdown, 1000);
+let questionEl = document.getElementById('question');
+let answerElA = document.getElementById('answerA');
+let answerElB = document.getElementById('answerB');
+let answerElC = document.getElementById('answerC');
+let answerElD = document.getElementById('answerD');
 
 function countdown() {
     if (timeLeft == 0) {
         clearTimeout(timerId);
         //insert run highscore page
     } else {
-        time.innerHTML = timeLeft + ' seconds remaining';
+        time.innerHTML = 'Timer: ' + timeLeft + ' seconds remaining';
         timeLeft--;
     }
-
 }
 
 //event listeners
-document.getElementById("backButton").addEventListener("click", function() {
-     alert("hi");
-     });
-document.getElementById("submitButton").addEventListener("click", function() {
+
+document.getElementById("submitButton").addEventListener("click", function () {
     alert('hi');
 });
+
+
+
+// question one: Question
+
+// A
+// B
+// C
+// D
+
+// if {
+//     // answer A is chosen, add 5 points to score
+// } else {
+//     // add 0 points to score && timeLeft - 5
+// };
+
+
+const questions = [
+    {
+        question: "Questions 1: What is not a data type?",
+        answers: [
+            "A) String",
+            "B) Boolean",
+            "C) Number",
+            "D) Character"],
+        correctAnswer: 'D'
+    }, {
+        question: "Questions 2: What is not a data type?",
+        answers: [
+            "A) String",
+            "B) Boolean",
+            "C) Number",
+            "D) Character"],
+        correctAnswer: 'D'
+    }, {
+        question: "Questions 3: What is not a data type?",
+        answers: [
+            "A) String",
+            "B) Boolean",
+            "C) Number",
+            "D) Character"],
+        correctAnswer: 'D'
+    }, {
+        question: "Questions 4: What is not a data type?",
+        answers: [
+            "A) String",
+            "B) Boolean",
+            "C) Number",
+            "D) Character"],
+        correctAnswer: 'D'
+
+    }, {
+        question: "Questions 5: What is not a data type?",
+        answers: [
+            "A) String",
+            "B) Boolean",
+            "C) Number",
+            "D) Character"],
+        correctAnswer: 'D'
+    }
+]
+
+let questionIndex = 0;
+renderQuestion(questionIndex);
+
+function nextQuestion() {
+    questionIndex++;
+    renderQuestion(questionIndex);
+}
+
+function renderQuestion (index) {
+    let currentQuestion = questions[index];
+    questionEl.textContent = currentQuestion.question;
+    answerElA.textContent = currentQuestion.answers[0];
+    answerElB.textContent = currentQuestion.answers[1];
+    answerElC.textContent = currentQuestion.answers[2];
+    answerElD.textContent = currentQuestion.answers[3];
+};
